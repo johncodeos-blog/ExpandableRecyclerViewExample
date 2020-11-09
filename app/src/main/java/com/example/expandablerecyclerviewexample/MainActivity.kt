9399b1894e7d092048c5e17d1a06e34958b5bad9
John Codeos <johncodeos@gmail.com>
2020-11-09 02:12:25 +0200

    New method to expand/collapse the cells without the height bug. Also added code to open only one cell at a time.

--- app/src/main/java/com/example/expandablerecyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/example/expandablerecyclerviewexample/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.expandablerecyclerviewexample
 
-import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -8,16 +7,16 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    var itemsData = ArrayList<DataModel>()
-    lateinit var adapter: RVAdapter
-    lateinit var mcontext: Context
+    private var itemsData = ArrayList<DataModel>()
+    private var expandedSize =  ArrayList<Int>()
+
+    private lateinit var adapter: RVAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mcontext = this.baseContext
 
-        adapter = RVAdapter(itemsData)
+        adapter = RVAdapter(itemsData, expandedSize)
         val llm = LinearLayoutManager(this)
 
         itemsrv.setHasFixedSize(true)
@@ -29,7 +28,18 @@ class MainActivity : AppCompatActivity() {
     private fun getData() {
         itemsData = ArrayList()
         itemsData = Data.items
+
+        setCellSize()
+
         adapter.notifyDataSetChanged()
-        adapter = RVAdapter(itemsData)
+        adapter = RVAdapter(itemsData, expandedSize)
+    }
+
+    // Set the expanded view size to 0, because all expanded views are collapsed at the beginning
+    private fun setCellSize() {
+        expandedSize = ArrayList()
+        for (i in 0 until itemsData.count()) {
+            expandedSize.add(0)
+        }
     }
 }
--- app/src/main/java/com/example/expandablerecyclerviewexample/RVAdapter.kt
+++ app/src/main/java/com/example/expandablerecyclerviewexample/RVAdapter.kt
@@ -1,51 +1,139 @@
 package com.example.expandablerecyclerviewexample
 
+import android.animation.ValueAnimator
 import android.content.Context
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.graphics.Typeface
+import android.os.Build
+import android.util.DisplayMetrics
+import android.util.Log
+import android.util.TypedValue
+import android.view.*
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.recyclerview_cell.view.*
 import java.util.*
 
 
-class RVAdapter(private val itemsCells: ArrayList<DataModel>) :
+class RVAdapter(
+    private val itemsCells: ArrayList<DataModel>,
+    private val expandedSize: ArrayList<Int>
+) :
     RecyclerView.Adapter<RVAdapter.ViewHolder>() {
 
-    // Save the expanded row position
-    private val expandedPositionSet: HashSet<Int> = HashSet()
-    lateinit var context: Context
+    private lateinit var context: Context
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
         val v =
             LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_cell, parent, false)
-        val vh = ViewHolder(v)
-        context = parent.context
-        return vh
+        return ViewHolder(v)
     }
 
     override fun getItemCount(): Int {
         return itemsCells.size
     }
 
+    private var lastTappedCell: Int? = null
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // Add data to cells
         holder.itemView.question_textview.text = itemsCells[position].question
         holder.itemView.answer_textview.text = itemsCells[position].answer
 
-        // Expand when you click on cell
-        holder.itemView.expand_layout.setOnExpandListener(object :
-            ExpandableLayout.OnExpandListener {
-            override fun onExpand(expanded: Boolean) {
-                if (expandedPositionSet.contains(position)) {
-                    expandedPositionSet.remove(position)
-                } else {
-                    expandedPositionSet.add(position)
-                }
+        // Set the height in answer TextView
+        holder.itemView.answer_textview.layoutParams.height = expandedSize[position]
+
+        // Expand/Collapse the answer TextView when you tap on the question TextView
+        holder.itemView.question_textview.setOnClickListener {
+
+
+            Log.d("last Tapped Cell is: ", lastTappedCell.toString())
+
+
+            if (expandedSize[position] == 0) {
+                // Calculate the height of the Answer Text
+                val answerTextViewHeight = height(
+                    context,
+                    itemsCells[position].answer,
+                    Typeface.DEFAULT,
+                    16,
+                    dp2px(15f, context)
+                )
+                changeViewSizeWithAnimation(
+                    holder.itemView.answer_textview,
+                    answerTextViewHeight,
+                    300L
+                )
+                expandedSize[position] = answerTextViewHeight
+            } else {
+                changeViewSizeWithAnimation(holder.itemView.answer_textview, 0, 300L)
+                expandedSize[position] = 0
             }
-        })
-        holder.itemView.expand_layout.setExpand(expandedPositionSet.contains(position))
+
+
+            // Uncomment the code below if you want to expand only one at a time
+//            if (lastTappedCell != null) {
+//                expandedSize[lastTappedCell!!] = 0
+//                notifyItemChanged(lastTappedCell!!)
+//            }
+//            lastTappedCell = position
+        }
     }
+
+    private fun changeViewSizeWithAnimation(view: View, viewSize: Int, duration: Long) {
+        val startViewSize = view.measuredHeight
+        val endViewSize: Int =
+            if (viewSize < startViewSize) (viewSize) else (view.measuredHeight + viewSize)
+        val valueAnimator =
+            ValueAnimator.ofInt(startViewSize, endViewSize)
+        valueAnimator.duration = duration
+        valueAnimator.addUpdateListener {
+            val animatedValue = valueAnimator.animatedValue as Int
+            val layoutParams = view.layoutParams
+            layoutParams.height = animatedValue
+            view.layoutParams = layoutParams
+        }
+        valueAnimator.start()
+    }
+
+    private fun height(
+        context: Context,
+        text: String,
+        typeface: Typeface?,
+        textSize: Int,
+        padding: Int
+    ): Int {
+        val textView = TextView(context)
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize.toFloat())
+        textView.setPadding(padding, padding, padding, padding)
+        textView.typeface = typeface
+        textView.text = text
+        val mMeasureSpecWidth =
+            View.MeasureSpec.makeMeasureSpec(getDeviceWidth(context), View.MeasureSpec.AT_MOST)
+        val mMeasureSpecHeight = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+        textView.measure(mMeasureSpecWidth, mMeasureSpecHeight)
+        return textView.measuredHeight
+    }
+
+    private fun dp2px(dpValue: Float, context: Context): Int {
+        val scale = context.resources.displayMetrics.density
+        return (dpValue * scale + 0.5f).toInt()
+    }
+
+    private fun getDeviceWidth(context: Context): Int {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val displayMetrics = DisplayMetrics()
+            val display: Display? = context.display
+            display?.getRealMetrics(displayMetrics)
+            displayMetrics.widthPixels
+        } else {
+            val wm = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+            val displayMetrics = DisplayMetrics()
+            wm.defaultDisplay.getMetrics(displayMetrics)
+            displayMetrics.widthPixels
+        }
+    }
+
 }
